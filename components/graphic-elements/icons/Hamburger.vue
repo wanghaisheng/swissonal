e1adaae34313efe1f77de6f5e38a6177623a9e4b
brunaFusiger <bruna@zytro.co.uk>
2024-07-03 19:10:26 +0200

    feat: :sparkles: add hamburger svg

---
+++ components/graphic-elements/icons/Hamburger.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+//fazer a animaçãoaqui dentro sem o X component!!!!!!!!!!
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 43 33" fill="none">
+    <g filter="url(#filter0_d_404_41)">
+      <rect x="1" y="2.00928" width="40" height="4" rx="2" fill="white" />
+      <rect x="0.5" y="1.50928" width="41" height="5" rx="2.5" stroke="#1E1E1E" />
+    </g>
+    <g filter="url(#filter1_d_404_41)">
+      <rect x="1" y="14.0093" width="40" height="4" rx="2" fill="white" />
+      <rect x="0.5" y="13.5093" width="41" height="5" rx="2.5" stroke="#1E1E1E" />
+    </g>
+    <g filter="url(#filter2_d_404_41)">
+      <rect x="1" y="26.0093" width="40" height="4" rx="2" fill="white" />
+      <rect x="0.5" y="25.5093" width="41" height="5" rx="2.5" stroke="#1E1E1E" />
+    </g>
+    <defs>
+      <filter id="filter0_d_404_41" x="0" y="1.00928" width="43" height="8" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feOffset dx="1" dy="2" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0.117647 0 0 0 0 0.117647 0 0 0 0 0.117647 0 0 0 1 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_404_41" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_404_41" result="shape" />
+      </filter>
+      <filter id="filter1_d_404_41" x="0" y="13.0093" width="43" height="8" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feOffset dx="1" dy="2" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0.117647 0 0 0 0 0.117647 0 0 0 0 0.117647 0 0 0 1 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_404_41" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_404_41" result="shape" />
+      </filter>
+      <filter id="filter2_d_404_41" x="0" y="25.0093" width="43" height="8" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feOffset dx="1" dy="2" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0.117647 0 0 0 0 0.117647 0 0 0 0 0.117647 0 0 0 1 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_404_41" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_404_41" result="shape" />
+      </filter>
+    </defs>
+  </svg>
+</template>
+
