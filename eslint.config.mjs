8c380aa5815464f22f8912fe22c908ddb96dae60
brunaFusiger <bruna@zytro.co.uk>
2024-07-06 19:51:31 +0200

    chore: :technologist: add one item per line

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import antfu from '@antfu/eslint-config'
 import unocss from '@unocss/eslint-config/flat'
 
 export default antfu({
+  rules: {
+    'vue/max-attributes-per-line': 1,
+  },
   stylistic: {
     indent: 2,
     quotes: 'single',
