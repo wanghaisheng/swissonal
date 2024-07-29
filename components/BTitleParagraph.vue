c42ba1cd7e39b1a23a55a90a9166a1bda5da723d
brunaFusiger <bruna@zytro.co.uk>
2024-07-29 10:05:30 +0200

    feat: :bug: remove text white fixed from the paragraph and make it or black or white

--- components/BTitleParagraph.vue
+++ components/BTitleParagraph.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 export interface Props {
   title: string
+  textWhite?: boolean
 }
 
 defineProps<Props>()
@@ -8,7 +9,10 @@ defineProps<Props>()
 
 <template>
   <div class="flex flex-col items-start gap-2">
-    <p class="text-white-100 paragraph-text-bold select-text selection:bg-white-100 selection:text-black-200">
+    <p
+      class="paragraph-text-bold select-text selection:bg-white-100 selection:text-black-200"
+      :class="textWhite ? 'text-white-100' : 'text-black-200'"
+    >
       {{ title }}
     </p>
     <slot />
