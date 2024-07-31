33f0ef69f2df6fd96cbaccdff74c3cea4bfdb8ec
brunaFusiger <bruna@zytro.co.uk>
2024-07-31 15:43:13 +0200

    feat: :sparkles: add benefits horizontal scroll

---
+++ components/BCardBenefit.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+export interface Props {
+  benefitImage: string
+  benefitTitle: string
+  benefitDescription: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div
+    class="min-w-50 sm:min-w-60 md:min-w-80 flex flex-col items-center gap-8 overflow-hidden rounded-md bg-white-100 px-4 py-4 text-center tracking-wide shadow-md"
+  >
+    <p
+      class="text-xl font-600 capitalize md:text-3xl"
+      v-text="benefitTitle"
+    />
+    <NuxtImg
+      class="h-32 w-32 select-none md:h-42 md:w-42"
+      :src="benefitImage"
+    />
+    <p
+      class="font-300 normal-case paragraph-text"
+      v-text="benefitDescription"
+    />
+  </div>
+</template>
--- pages/benefits.vue
+++ pages/benefits.vue
@@ -1,8 +1,95 @@
 <script setup lang="ts">
+import gsap from 'gsap'
+
+interface Item {
+  image: string
+  title: string
+  description: string
+}
+
+const { locale } = useI18n()
+
+const benefitItems = ref<Item[]>([])
+
+async function loadData() {
+  const data = await queryContent(`${locale.value}/benefits`).find()
+  benefitItems.value = data[0]?.benefits
+}
+
+const containerCards = ref<HTMLDivElement | null>(null)
+
+onMounted(() => {
+  loadData().then(() => {
+    const cards = document.querySelectorAll('.card')
+
+    gsap.to(cards, {
+      xPercent: -100 * (cards.length - 1),
+      ease: 'none',
+      scrollTrigger: {
+        trigger: '#benefitCardsSection',
+        pin: true,
+        pinSpacing: true,
+        scrub: 1,
+        snap: 1 / (cards.length - 1),
+        end: () => `+=${containerCards.value?.offsetWidth}`,
+      },
+    })
+  })
+})
 </script>
 
 <template>
-  <div>
-    benefits
+  <div class="selection:text-white-200 w-dvw flex flex-col items-center justify-center selection:bg-red-100">
+    <section
+      class="section-full-width-aligment-parent flex flex-col items-center bg-green-100 selection:bg-white-100 selection:text-black-200"
+    >
+      <div class="section-full-width-aligment-child-first-section">
+        <BMainTitleSubtitle
+          :title="$t('benefits.title')"
+          :subtitle="$t('benefits.subtitle')"
+        />
+      </div>
+    </section>
+
+    <section
+      id="benefitCardsSection"
+      class="relative z-10 h-max h-screen flex items-center overflow-x-hidden bg-green-100 md:p-0"
+    >
+      <div
+        ref="containerCards"
+        class="container-cards relative inset-x-5 mx-10 w-dvw flex flex-row flex-nowrap justify-start gap-4 md:left-30% sm:left-10% md:mr-0"
+      >
+        <BCardBenefit
+          v-for="(item, index) in benefitItems"
+          :key="index"
+          class="card"
+          :benefit-title="item.title"
+          :benefit-description="item.description"
+          :benefit-image="item.image"
+        />
+      </div>
+    </section>
+
+    <div class="max-w-[1224px] w-full grow">
+      <BScrollingTicker class="my-20" />
+    </div>
+
+    <section class="section-full-width-aligment-parent">
+      <div
+        v-once
+        class="mb-20 max-w-[1024px] flex grow flex-col items-center gap-8 px-8 md:flex-row md:items-start md:justify-between md:gap-4"
+      >
+        <BCardImageLink
+          image-path="/images/calendar.webp"
+          :text="$t('card-links.calendar')"
+          link="/"
+        />
+        <BCardImageLink
+          image-path="/images/planting-tomatos.webp"
+          :text="$t('card-links.about')"
+          link="/about"
+        />
+      </div>
+    </section>
   </div>
 </template>
