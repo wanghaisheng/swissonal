96803d165db5c096197180abe8631eb80a67869f
brunaFusiger <bruna@zytro.co.uk>
2024-07-31 16:07:26 +0200

    feat: :sparkles: make card bigger in big screens

--- components/BCardBenefit.vue
+++ components/BCardBenefit.vue
@@ -10,14 +10,14 @@ defineProps<Props>()
 
 <template>
   <div
-    class="min-w-50 sm:min-w-60 md:min-w-80 flex flex-col items-center gap-8 overflow-hidden rounded-md bg-white-100 px-4 py-4 text-center tracking-wide shadow-md"
+    class="min-w-50 flex flex-col items-center gap-8 overflow-hidden rounded-md bg-white-100 px-4 py-4 text-center tracking-wide shadow-md lg:min-w-120 md:min-w-80 sm:min-w-60 lg:py-16"
   >
     <p
-      class="text-xl font-600 capitalize md:text-3xl"
+      class="text-xl font-600 capitalize lg:text-4xl md:text-3xl"
       v-text="benefitTitle"
     />
     <NuxtImg
-      class="h-32 w-32 select-none md:h-42 md:w-42"
+      class="h-32 w-32 select-none lg:h-64 lg:w-64 md:h-42 md:w-42"
       :src="benefitImage"
     />
     <p
