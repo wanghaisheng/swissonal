678bf3c05628cb9331fae403c48b66882e18b329
brunaFusiger <bruna@zytro.co.uk>
2024-08-07 15:55:09 +0200

    perf: :zap: add lazy to cursor bee

--- app.vue
+++ app.vue
@@ -29,7 +29,7 @@ callOnce(() => {
 <template>
   <NuxtLayout>
     <BPageLoader />
-    <BCustomCursor class="hidden 2lg:block" />
+    <LazyBCustomCursor class="hidden 2lg:block" />
     <NuxtPage />
   </NuxtLayout>
 </template>
